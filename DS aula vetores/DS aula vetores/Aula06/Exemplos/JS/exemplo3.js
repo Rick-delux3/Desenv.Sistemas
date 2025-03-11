03aa2f12c4fae3f2e5cd62c72b7df21144f65244
HENRIQUE <henriquerodriguesneves4@gmail.com>
2025-03-11 15:05:33 -0300

    DS1

---
+++ DS aula vetores/DS aula vetores/Aula06/Exemplos/JS/exemplo3.js
@@ -0,0 +1,18 @@
+var frutas = ["morango", "maça", "uva"]
+
+for(var i=0;i<frutas.length;i++){
+    document.write(frutas[i]+"<br>");
+}
+document.write("_______<br>");
+//adicionando elemento no inicio do vetor com a função unshift()
+frutas.unshift("pitaya");
+for(var i=9; i<frutas.length; i++){
+    document.write(frutas[i]+"<br>");
+}
+document.write("_______<br>");
+
+//removendo o último elemento do vetor com a função pop()
+frutas.pop();
+for(var i=0;i<frutas.length;i++){
+    document.write(frutas[i]+"<br>")
+}
