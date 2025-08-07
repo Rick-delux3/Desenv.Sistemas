e264323b60c572c9b056096df38466d34ecc0fcd
Rick-delux3 <henriquerodriguesneves4@gmail.com>
2025-08-07 15:42:58 -0300

    Atividade

---
+++ 2_Semestre/3_Bimestre/Class_JS/Atividade_de_revisão/main.js
@@ -0,0 +1,6 @@
+const vei =[new Veiculo, new Carro]
+
+vei.marca = prompt("Dgite a marca do véiculo");
+vei.modelo = prompt("Digite o modelo do veículo");
+vei.ano = Number(prompt("Digite o ano do veículo!"));
+vei.portas = prompt("Digite a quantidade de portas!");
