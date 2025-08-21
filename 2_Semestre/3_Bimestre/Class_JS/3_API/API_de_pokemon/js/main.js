96153087b0a2847c78aabf74a9f23252dc22d1c0
Rick-delux3 <henriquerodriguesneves4@gmail.com>
2025-08-21 16:21:23 -0300

    pokemon2

--- 2_Semestre/3_Bimestre/Class_JS/3_API/API_de_pokemon/js/main.js
+++ 2_Semestre/3_Bimestre/Class_JS/3_API/API_de_pokemon/js/main.js
@@ -1,7 +1,9 @@
-function buscarPokemon(){
-    let pkm_name = document.querySelector("#pkm_name").value;
+function buscarPokemon1(){
+    let pkm_name1 = document.querySelector("#pkm_name1").value;
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pkm_name}`)
+
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pkm_name1}`)
     .then(resposta =>{
         if(!resposta.ok){
             throw new Error("Pokemon não encontrado");
@@ -17,13 +19,51 @@ function buscarPokemon(){
 
         console.log(p.exibirDados());
 
-        document.getElementById("resultado").innerHTML = 
+        document.getElementById("resultado1").innerHTML = 
         `<h1>${p.nome}</h1>
         <ul>
             <li>Tipo: ${p.tipo} </li>
             <li>Peso: ${p.peso} </li>
         </ul>
             <img src="${p.sprite}">
+            `
+            
+            
+        
+    })
+    .catch(erro=>{
+        alert(erro.message);
+    })
+}
+
+function buscarPokemon2(){
+    let pkm_name2 = document.querySelector("#pkm_name2").value;
+
+
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pkm_name2}`)
+    .then(resposta =>{
+        if(!resposta.ok){
+            throw new Error("Pokemon não encontrado");
+        }
+        return resposta.json();
+    })
+    .then(dados =>{
+        const po = new Pokemon();
+        po.nome = dados.name;
+        po.tipo = dados.types.map(t=> t.type.name);
+        po.peso = dados.weight;
+        po.sprite = dados.sprites.front_default;
+
+        console.log(po.exibirDados());
+
+        document.getElementById("resultado2").innerHTML = 
+        `<h1>${po.nome}</h1>
+        <ul>
+            <li>Tipo: ${po.tipo} </li>
+            <li>Peso: ${po.peso} </li>
+        </ul>
+            <img src="${po.sprite}">
             `
             
             
