f5ce76801997ca68330195a0fd12499fcb768375
Rick-delux3 <henriquerodriguesneves4@gmail.com>
2025-09-23 11:25:03 -0300

    Pokemon_Fight2.2

--- 2_Semestre/3_Bimestre/3_Projetos/API_de_pokemon/js/classes/treinadores.js
+++ 2_Semestre/3_Bimestre/3_Projetos/API_de_pokemon/js/classes/treinadores.js
@@ -2,12 +2,13 @@ class Treinador{
 
     static lista = [];
 
-    constructor(nome, regiao, bio, genero){
+    constructor(nome, regiao, bio, genero, idade){
         this.id = crypto.randomUUID();
         this.nome = nome;
         this.regiao = regiao;
         this.bio = bio;
         this.genero = genero;
+        this.idade = idade
 
     };
 
@@ -19,7 +20,7 @@ class Treinador{
         const dados = JSON.parse(localStorage.getItem("treinadores")) || [];
         
         Treinador.lista = dados.map(t =>{
-            const objet = new Treinador(t.nome, t.regiao, t.bio, t.genero);
+            const objet = new Treinador(t.nome, t.regiao, t.bio, t.genero, t.idade);
             objet.id = t.id;
             return objet
         });
@@ -32,13 +33,14 @@ class Treinador{
         return `<div class="card-treinador col-7">
                     <h1 id="nome">${this.nome}</h1>
                     <div class="dados d-flex flex-column">
+                        <p id="idade"><strong>Idade:</strong> ${this.idade}</p>
                         <p><strong>Região:</strong> ${this.regiao}</p>
                         <p id="biograf"><strong>Biografia:</strong> ${this.bio}</p>
                         <p><strong>Genero:</strong> ${this.genero}</p>
                     </div>
-                    <div class="acoes">
-                        <button onclick="Treinador.editar('${this.id}')">Atualizar</button>
-                        <button onclick="Treinador.deletar('${this.id}')">Deletar</button>
+                    <div class="acoes d-flex justify-content-around align-content-center">
+                        <button class="btn1" onclick="Treinador.editar('${this.id}')">Atualizar</button>
+                        <button class="btn2" onclick="Treinador.deletar('${this.id}')">Deletar</button>
                     </div>
                 </div>`;
     }
@@ -53,12 +55,14 @@ class Treinador{
         Treinador.salvarLocalStorage();
     }
     static editar(id){
+        Treinador.carregarLocalStorage();
         
         const treinador = Treinador.lista.find(t => t.id === id);
 
         if(treinador){
             let h1 = document.getElementById("nome");
             let p = document.getElementById("biograf");
+            let p2 = document.getElementById("idade");
             
 
             const input = document.createElement("input");
@@ -72,11 +76,23 @@ class Treinador{
             textarea.placeholder = "Digite a nova Biografia";
             textarea.value = treinador.bio;
 
+            const input2 = document.createElement("input");
+            input2.type = "Number"
+            input2.name = "idade";
+            input2.placeholder = "Digite a nova idade";
+            input2.value = treinador.idade;
+
+            
             h1.innerHTML = "";
             h1.appendChild(input);
 
             p.innerHTML = "";
             p.appendChild(textarea);
+
+            p2.innerHTML = "";
+            p.appendChild(input2);
+
+            
 
             input.addEventListener("keydown", function(e){
                 if(e.key === "Enter"){
@@ -89,6 +105,14 @@ class Treinador{
             textarea.addEventListener("keydown", function(e){
                 if(e.key === "Enter"){
                     treinador.bio = textarea.value;
+
+                    Treinador.salvarLocalStorage();
+                    Treinador.atualizarTela();
+                }
+            })
+            input2.addEventListener("keydown", function(e){
+                if(e.key === "Enter"){
+                    treinador.idade = input2.value;
 
                     Treinador.salvarLocalStorage();
                     Treinador.atualizarTela();
--- 2_Semestre/3_Bimestre/3_Projetos/API_de_pokemon/js/mains/main_cadastro.js
+++ 2_Semestre/3_Bimestre/3_Projetos/API_de_pokemon/js/mains/main_cadastro.js
@@ -6,13 +6,14 @@ document.getElementById("formTreinador").addEventListener("submit", function(e){
     const nome = document.getElementById("name").value;
     const regiao = document.getElementById("regiao").value;
     const bio = document.getElementById("bio").value;
-    const gender = document.getElementById("gender").value
+    const gender = document.getElementById("gender").value;
+    const y = document.getElementById("idade").value;
 
-    const novoTreinador = new Treinador(nome, regiao, bio, gender);
+    const novoTreinador = new Treinador(nome, regiao, bio, gender, y);
     Treinador.adicionarTreinador(novoTreinador);
     e.target.reset();
 
-    alert("Treinador Cadastrado com sucesso")
+    alert("Treinador Cadastrado com sucesso!!");
 });
 
 
